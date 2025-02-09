ed010d3de022f019e9b3985046504d0d888afd3d
Emmanuel Daniels <edaniels@pltw.org>
2025-02-09 00:12:52 -0500

    added Connect to Coinbase & Fetch Real-Time Data

--- python crypto_bot.py
+++ python crypto_bot.py
@@ -4,3 +4,16 @@ import os
 load_dotenv()
 COINBASE_API_KEY = os.getenv("COINBASE_API_KEY")
 COINBASE_API_KEY = os.getenv("COINBASE_API_SECRET")
+
+# Connect to Coinbase & Fetch Real-Time Data
+
+from coinbase.wallet.client import Client
+
+client = Client(COINBASE_API_KEY, COINBASE_API_SECRET)
+
+def get_price(currency_pair="BTC-USD"):
+    rice = client.get_spot_price(currency_pair=currency_pair)
+    return price['amount']
+
+print(get_price())
+
